bc77da91c108f672cf108873da09fa7177a67684
rangerboy574 <jdeweese574@gmail.com>
2022-12-26 11:57:38 -0600

    Initial Commit

---
+++ Objectives/ObjectivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectivesApp.swift
+//  Objectives
+//
+//  Created by Joseph  DeWeese on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ObjectivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
